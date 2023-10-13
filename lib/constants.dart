25aeb37f5b6f124634743aec889186a6f68c48ea
Prateek Thakur <prateekthakur272@gmail.com>
2023-10-14 01:38:48 +0530

    adds home screen

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+// Strings
+import 'package:flutter/material.dart';
+
+const String title = 'Calculator';
+
+// Colors
+const numberButtonColor = Color.fromARGB(0, 43, 43, 43);
+const operationButtonColor = Color.fromARGB(0, 43, 43, 43);
+const otherButtonColor = Color.fromARGB(0, 43, 43, 43);
