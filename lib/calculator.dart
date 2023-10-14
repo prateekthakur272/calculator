9acf71cefe96b1fc55a02e77fca90f26cdf94194
Prateek Thakur <prateekthakur272@gmail.com>
2023-10-14 16:56:07 +0530

    adds basic layout

---
+++ lib/calculator.dart
@@ -0,0 +1,7 @@
+class Calculator {
+  static num? a;
+  static num? b;
+  static num? res;
+  static String? op;
+  static List<String> stack = List.empty(growable: true);
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,64 @@
 import 'package:calculator/constants.dart';
-import 'package:calculator/widgets/circle_button.dart';
-import 'package:calculator/widgets/stadium_button.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String display = '0';
+
+  circleButton({
+    required label,
+    required backgroundColor,
+    required foregroundColor,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.all(6.0),
+      child: FilledButton(
+        style: FilledButton.styleFrom(
+            padding: const EdgeInsets.all(8),
+            minimumSize: const Size(100, 100),
+            shape: const CircleBorder(),
+            textStyle:
+                const TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            backgroundColor: backgroundColor,
+            foregroundColor: foregroundColor),
+        onPressed: () {
+          setState(() {});
+        },
+        child: Text(
+          label,
+        ),
+      ),
+    );
+  }
+
+  stadiumButton(
+      {required label, required backgroundColor, required foregroundColor}) {
+    return Padding(
+      padding: const EdgeInsets.all(6.0),
+      child: FilledButton(
+        style: FilledButton.styleFrom(
+            padding: const EdgeInsets.all(8),
+            minimumSize: const Size(100, 100),
+            shape: const StadiumBorder(),
+            textStyle:
+                const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            backgroundColor: backgroundColor,
+            foregroundColor: foregroundColor),
+        onPressed: () {
+          setState(() {});
+        },
+        child: Text(
+          label,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,41 +71,40 @@ class HomeScreen extends StatelessWidget {
         children: [
           Expanded(
             flex: 1,
-            child: Container(
-              child: const Align(
-                  alignment: Alignment.bottomRight,
-                  child: Text(
-                    '100',
-                    style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
-                  )),
-            ),
+            child: Align(
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  display,
+                  style: const TextStyle(
+                      fontSize: 100, fontWeight: FontWeight.bold),
+                )),
           ),
-          const Expanded(
+          Expanded(
             flex: 3,
             child: Column(
               children: [
                 Row(
                   children: [
                     Expanded(
-                      child: CircleButton(
+                      child: circleButton(
                           label: 'AC',
                           backgroundColor: otherButtonColor,
                           foregroundColor: Colors.black),
                     ),
                     Expanded(
-                      child: CircleButton(
+                      child: circleButton(
                           label: '+/-',
                           backgroundColor: otherButtonColor,
                           foregroundColor: Colors.black),
                     ),
                     Expanded(
-                      child: CircleButton(
+                      child: circleButton(
                           label: '%',
                           backgroundColor: otherButtonColor,
                           foregroundColor: Colors.black),
                     ),
                     Expanded(
-                      child: CircleButton(
+                      child: circleButton(
                           label: '/',
                           backgroundColor: operationButtonColor,
                           foregroundColor: Colors.white),
@@ -61,25 +114,25 @@ class HomeScreen extends StatelessWidget {
                 Row(
                   children: [
                     Expanded(
-                      child: CircleButton(
+                      child: circleButton(
                           label: '7',
                           backgroundColor: numberButtonColor,
                           foregroundColor: Colors.white),
                     ),
                     Expanded(
-                      child: CircleButton(
+                      child: circleButton(
                           label: '8',
                           backgroundColor: numberButtonColor,
                           foregroundColor: Colors.white),
                     ),
                     Expanded(
-                      child: CircleButton(
+                      child: circleButton(
                           label: '9',
                           backgroundColor: numberButtonColor,
                           foregroundColor: Colors.white),
                     ),
                     Expanded(
-                      child: CircleButton(
+                      child: circleButton(
                           label: 'X',
                           backgroundColor: operationButtonColor,
                           foregroundColor: Colors.white),
@@ -89,25 +142,25 @@ class HomeScreen extends StatelessWidget {
                 Row(
                   children: [
                     Expanded(
-                      child: CircleButton(
+                      child: circleButton(
                           label: '4',
                           backgroundColor: numberButtonColor,
                           foregroundColor: Colors.white),
                     ),
                     Expanded(
-                      child: CircleButton(
+                      child: circleButton(
                           label: '5',
                           backgroundColor: numberButtonColor,
                           foregroundColor: Colors.white),
                     ),
                     Expanded(
-                      child: CircleButton(
+                      child: circleButton(
                           label: '6',
                           backgroundColor: numberButtonColor,
                           foregroundColor: Colors.white),
                     ),
                     Expanded(
-                      child: CircleButton(
+                      child: circleButton(
                           label: '-',
                           backgroundColor: operationButtonColor,
                           foregroundColor: Colors.white),
@@ -117,25 +170,25 @@ class HomeScreen extends StatelessWidget {
                 Row(
                   children: [
                     Expanded(
-                      child: CircleButton(
+                      child: circleButton(
                           label: '1',
                           backgroundColor: numberButtonColor,
                           foregroundColor: Colors.white),
                     ),
                     Expanded(
-                      child: CircleButton(
+                      child: circleButton(
                           label: '2',
                           backgroundColor: numberButtonColor,
                           foregroundColor: Colors.white),
                     ),
                     Expanded(
-                      child: CircleButton(
+                      child: circleButton(
                           label: '3',
                           backgroundColor: numberButtonColor,
                           foregroundColor: Colors.white),
                     ),
                     Expanded(
-                      child: CircleButton(
+                      child: circleButton(
                           label: '+',
                           backgroundColor: operationButtonColor,
                           foregroundColor: Colors.white),
@@ -146,18 +199,18 @@ class HomeScreen extends StatelessWidget {
                   children: [
                     Expanded(
                         flex: 2,
-                        child: StadiumButton(
+                        child: stadiumButton(
                             label: '0',
                             backgroundColor: numberButtonColor,
                             foregroundColor: Colors.white)),
                     Expanded(
-                      child: CircleButton(
+                      child: circleButton(
                           label: '.',
                           backgroundColor: numberButtonColor,
                           foregroundColor: Colors.white),
                     ),
                     Expanded(
-                      child: CircleButton(
+                      child: circleButton(
                           label: '=',
                           backgroundColor: operationButtonColor,
                           foregroundColor: Colors.white),
